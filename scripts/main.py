c96fbc668fef3821e3c67bd4a87109574e6a53f0
jun <jun.lee3@baruchmail.cuny.edu>
2024-05-14 20:51:06 -0400

    adding main.py

---
+++ scripts/main.py
@@ -0,0 +1,14 @@
+import json
+
+# specify the path to your Json configuration file
+config_file_path = 'config/config.json'
+
+
+# load the JSON configuration file
+with open(config_file_path, 'r') as config_file:
+    config = json.load(config_file)
+
+#print the configuration
+
+Connection_STRING = print(config['connectionString'])
+
